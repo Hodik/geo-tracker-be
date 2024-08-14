fdb17cee10f36621c5130b4b9d6d36bd8e16dc9d
denis.khodishchenko <denys@hennessey.com>
2024-08-14 14:51:08 +0300

    fix

--- messaging/validateWebhook.go
+++ messaging/validateWebhook.go
@@ -1,21 +1,26 @@
 package messaging
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/twilio/twilio-go/client"
 )
 
-func ValidateWebhook(c *gin.Context) bool {
+func ValidateWebhook(c *gin.Context) error {
 	var params map[string]string
 
 	requestValidator := client.NewRequestValidator(TwilioAuthToken)
 
 	if err := c.ShouldBindJSON(&params); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return false
+		return err
 	}
 
 	signature := c.GetHeader("X-Twilio-Signature")
 
-	return requestValidator.Validate(WebhookUrl, params, signature)
+	if !requestValidator.Validate(WebhookUrl, params, signature) {
+		return errors.New("invalid signature")
+	}
+
+	return nil
 }
