6b819c15fb01ec3be1a9b060753bf1d2fffd2215
Hodik <d.khodishchenko@gmail.com>
2024-08-27 11:42:07 +0300

    github actions

--- terraform/05_rds.tf
+++ terraform/05_rds.tf
@@ -34,6 +34,13 @@ resource "aws_security_group" "rds_prod" {
     security_groups = [aws_security_group.ecs_security_group.id]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = "5432"
+    to_port     = "5432"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
