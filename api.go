56d2395f59c3a0e079f6b905e43a8d98d38e7dd3
denis.khodishchenko <denys@hennessey.com>
2024-09-02 18:00:30 +0300

    authorization + user management

--- api.go
+++ api.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/Hodik/geo-tracker-be/auth"
+	"github.com/Hodik/geo-tracker-be/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +15,24 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(middleware.EnsureValidToken())
+	r.Use(auth.FetchOrCreateUser(db))
+
+	r.GET("/protected", func(c *gin.Context) {
+		token := c.MustGet("token")
+
+		log.Default().Println(token)
+		// Use the token claims as needed
+
+		user := c.MustGet("user").(*auth.User)
+
+		log.Default().Println(user)
+		c.JSON(http.StatusOK, gin.H{"message": "You have access!", "claims": token})
+	})
+
+	r.GET("/me", getMe)
+	r.PATCH("/me", updateMe)
 	r.GET("/devices", getGPSDevices)
 	r.POST("/devices", createGPSDevice)
 	r.PATCH("/devices/:id", updateGPSDevice)
