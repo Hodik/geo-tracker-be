366c415ce7b3d1d5be74831cd30b2065431d62e0
denis.khodishchenko <denys@hennessey.com>
2024-09-12 13:29:10 +0300

    communities start

--- views.go
+++ views.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"log"
 
 	"github.com/Hodik/geo-tracker-be/auth"
 	"github.com/gin-gonic/gin"
@@ -17,6 +16,7 @@ func getGPSDevices(c *gin.Context) {
 }
 
 func createGPSDevice(c *gin.Context) {
+	user := c.MustGet("user").(*auth.User)
 	var device CreateGPSDevice
 
 	if err := c.ShouldBindJSON(&device); err != nil {
@@ -24,14 +24,8 @@ func createGPSDevice(c *gin.Context) {
 		return
 	}
 
-	deviceModel := device.ToGPSDevice()
-
-	log.Println("BEFORE:", deviceModel.Tracking)
-
+	deviceModel := device.ToGPSDevice(user)
 	db.Create(&deviceModel)
-
-	log.Println("AFTER CREATE:", deviceModel.Tracking)
-
 	c.JSON(201, deviceModel)
 }
 
@@ -115,4 +109,120 @@ func updateMe(c *gin.Context) {
 	db.Save(userSettings)
 
 	c.JSON(200, ToUserProfile(user, userSettings))
+}
+
+func createAreaOfInterest(c *gin.Context) {
+	user := c.MustGet("user").(*auth.User)
+	var schema CreateAreaOfInterest
+
+	if err := c.ShouldBindJSON(&schema); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	aoiModel, err := schema.ToAreaOfInterest(user)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Create(&aoiModel)
+	c.JSON(201, aoiModel)
+}
+
+func getMyAreasOfInterest(c *gin.Context) {
+	user := c.MustGet("user").(*auth.User)
+	var aois []AreaOfInterest
+	result := db.Raw("SELECT id, user_id, ST_AsText(polygon_area) as polygon_area, created_at, updated_at, deleted_at FROM area_of_interests WHERE user_id = ?", user.ID).Scan(&aois)
+
+	if result.Error != nil {
+		c.JSON(500, gin.H{"error": result.Error})
+	}
+	c.JSON(200, aois)
+}
+
+func createCommunity(c *gin.Context) {
+	user := c.MustGet("user").(*auth.User)
+	var schema CreateCommunity
+
+	if err := c.ShouldBindJSON(&schema); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	comminuty, err := schema.ToCommunity(user)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Create(&comminuty)
+	c.JSON(201, comminuty)
+}
+
+func updateCommunity(c *gin.Context) {
+	user := c.MustGet("user").(*auth.User)
+	var schema UpdateCommunity
+
+	if err := c.ShouldBindJSON(&schema); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Param("id")
+
+	var community Community
+	result := db.First(&community, id)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		c.JSON(404, gin.H{"error": "community not found"})
+		return
+	}
+
+	comminuty, err := schema.ToCommunity(&community, user)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Save(&comminuty)
+	c.JSON(201, comminuty)
+}
+
+func getCommunity(c *gin.Context) {
+	id := c.Param("id")
+
+	var community Community
+	result := db.Select("communities.created_at, communities.deleted_at, communities.updated_at, communities.id, communities.name, communities.description, ST_AsText(polygon_area) AS polygon_area, type, admin_id").Preload("Members", func(db *gorm.DB) *gorm.DB {
+		return db.Order("created_at DESC")
+	}).Preload("Events", func(db *gorm.DB) *gorm.DB {
+		return db.Order("created_at DESC")
+	}).Joins("Admin").First(&community, id)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		c.JSON(404, gin.H{"error": "community not found"})
+		return
+	}
+
+	if result.Error != nil {
+		c.JSON(500, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(200, community)
+}
+
+func getCommunities(c *gin.Context) {
+	var communities []Community
+	result := db.Select("communities.created_at, communities.deleted_at, communities.updated_at, communities.id, communities.name, communities.description, ST_AsText(polygon_area) AS polygon_area, type, admin_id").Find(&communities)
+
+	if result.Error != nil {
+		c.JSON(500, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(200, communities)
 }
